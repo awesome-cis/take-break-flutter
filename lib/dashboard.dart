76c35415ab7d73e1db0fc0dad0e1d93a27220332
oyeahhh <oyeahhh@naver.com>
2019-12-17 21:59:43 +0900

    add fetch item from mockapi

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,56 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class Dashboard extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Item> fetchItems() async {
+  final String API_URL = 'https://5cbe1ec6ecded20014c20e48.mockapi.io';
+  final response = await http.get('${API_URL}/dashboard');
+
+  print(response.body);
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+    return Item.hi(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Item {
+  final int allPointsAmount;
+  final int availablePointsAmount;
+  final int usedPointsAmount;
+  final List<Map<String, dynamic>> usedPoints;
+
+  Item({this.allPointsAmount, this.availablePointsAmount, this.usedPointsAmount, this.usedPoints});
+
+  factory Item.hi(Map<String, dynamic> json) {
+    return Item(
+      allPointsAmount: json['allPointsAmount'],
+      availablePointsAmount: json['availablePointsAmount'],
+      usedPointsAmount: json['usedPointsAmount'],
+      usedPoints: json['usedPoints'],
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  Future<Item> items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = fetchItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,3 +68,4 @@ class Dashboard extends StatelessWidget {
     );
   }
 }
+
